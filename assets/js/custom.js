9e214930672cbb4d176736ba16a6c3a1c2a509f2
intellegence0706 <f.engineer.f7@gmail.com>
2024-09-27 02:26:37 +0900

    fix: all 4th

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,10 +2,9 @@
 function changeBackgroundColor() {
     var element = document.getElementById('l-header');
     element.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
-    console.log("adfadsfadsf");
 }
 
 // Event listener for scroll event
 window.addEventListener('scroll', function() {
     changeBackgroundColor();
-});
+}, { passive: true });
