ba92e2d31c3e76f44db35e114f859ed416b0820b
intellegence0706 <f.engineer.f7@gmail.com>
2024-09-30 02:49:54 +0900

    fix: update line

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,10 @@
 // Function to change background color
-function changeBackgroundColor() {
-    var element = document.getElementById('l-header');
-    element.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
-}
+// function changeBackgroundColor() {
+//     var element = document.getElementById('l-header');
+//     element.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+// }
 
 // Event listener for scroll event
-window.addEventListener('scroll', function() {
-    changeBackgroundColor();
-}, { passive: true });
+// window.addEventListener('scroll', function() {
+//     changeBackgroundColor();
+// }, { passive: true });
