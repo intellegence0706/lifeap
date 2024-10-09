db490b20a3c82f082d4e075725f41d278c33fafe
intellegence0706 <f.engineer.f7@gmail.com>
2024-10-09 11:19:08 +0900

    fix: footer link

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,3 +8,20 @@
 // window.addEventListener('scroll', function() {
 //     changeBackgroundColor();
 // }, { passive: true });
+// Select the div with the class '__contact'
+const contactDiv = document.querySelector('.__contact');
+
+// Add a click event listener to the div
+contactDiv.addEventListener('click', function() {
+    // Redirect to the contact.html page
+    window.location.href = 'contact.html';
+});
+
+const careerDiv = document.querySelector('.__career');
+
+// Add a click event listener to the div
+careerDiv.addEventListener('click', function() {
+    // Redirect to the career.html page
+    window.location.href = 'about.html';
+});
+
